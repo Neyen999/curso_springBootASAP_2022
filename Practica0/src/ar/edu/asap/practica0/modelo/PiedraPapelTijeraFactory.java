a7e9d56441d2c6d65c0b59713cf1b49434dced83
Neyen Marinelli <neyen_gt@hotmail.com>
2022-03-28 18:32:17 -0300

    Agregado de las clases Piedra, Papel y Tijera

--- Practica0/src/ar/edu/asap/practica0/modelo/PiedraPapelTijeraFactory.java
+++ Practica0/src/ar/edu/asap/practica0/modelo/PiedraPapelTijeraFactory.java
@@ -12,4 +12,40 @@ public abstract class PiedraPapelTijeraFactory {
 	
 	protected String nombre;
 	protected int numero;
+	
+	public PiedraPapelTijeraFactory(String nombre, int numero) {
+		super();
+		this.nombre = nombre;
+		this.numero = numero;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getDescripcionResultado() {
+		return descripcionResultado;
+	}
+	
+	public abstract boolean itsMe(int pNum);
+	public abstract int comparar(PiedraPapelTijeraFactory pPTF);
+	
+	public static PiedraPapelTijeraFactory getInstance(int pNum) {
+		// TODO hay que completar esta función que retorna una instancia de PPTF
+		// Acá hay que armar el código
+		return null;
+	};
+	
 }
